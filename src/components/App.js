ee81567a5f278651ae2b3a872733c24045d21027
okFox <smayfield@gmail.com>
2020-01-29 09:29:32 -0800

    refactored header, footer, and css for both

--- src/components/App.js
+++ src/components/App.js
@@ -2,54 +2,19 @@ import React, { Component } from 'react';
 import styles from './App.css';
 // import Character from './Character';
 import MapCharacters from './MapCharacters';
-
-class Header extends Component {
-  render() {
-    return (
-      <header className={styles.Header}>
-        <nav>
-          <ul>
-            <li><a className={styles.active} href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Documentation</a></li>
-          </ul>
-        </nav>
-        <section>
-          <h1>The Rick And Morty Api</h1>
-          <h2>Hey, did you ever want to hold a Terry fold?</h2>
-        </section>
-      </header>
-    );
-  }
-}
-
-class Footer extends Component {
-  render() {
-    return (
-      <footer className={styles.Footer}>
-        <section className={styles.stats}>
-          <p>Characters: 493</p>
-          <p>Locations: 76</p>
-          <p>Episodes: 31</p>
-        </section>
-        <section className={styles.acknowledgment}>
-          <p>❮❯ by <a href="#">Axel Fuhrmann</a> 2020</p>
-        </section>
-      </footer>
-    );
-  }
-}
+import Header from './Header';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
+      <>
         <Header /> 
         <div className={styles.cardsContainer}> 
           {MapCharacters}
         </div>
         <Footer />
-      </React.Fragment>
+      </>
     );
   }
 }
